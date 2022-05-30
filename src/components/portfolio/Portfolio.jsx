a97261b039836d4b0172f3274e339645e273f102
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-05-30 16:40:24 -0300

    build: first release.

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+const Portfolio = () => {
+
+    return (
+        <section id="portfolio">
+        <h5>Conheça meus</h5>
+            <h2>Projetos</h2>
+        </section>
+    )
+}
+
+export default Portfolio
